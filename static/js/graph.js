0d5d721db76fc7add99b408cd39cd9e99c65e7ae
plyn85 <plyn99@gmail.com>
2019-10-22 22:02:17 +0100

    adding the first line graph an csv data set up

--- static/js/graph.js
+++ static/js/graph.js
@@ -6,3 +6,47 @@ $(document).ready(function() {
     $("#callout_text").hide();
   });
 });
+//end of jquery
+
+//
+//main graph buliding section
+//gobal var added here will be passed into tickforamt
+//function to change amounts to euros
+var euroFormat = function(d) {
+  return "€" + d3.format(".2s")(d);
+};
+//calling csv data here then pssing though crossfilter function
+d3.csv("data/data.csv").then(function(sportData) {
+  var ndx = crossfilter(sportData);
+
+  //passing crossfiltered data into function that will then be rendered below
+  showAverageOnLineChart(ndx);
+
+  dc.renderAll();
+});
+
+//line graph function
+function showAverageOnLineChart(ndx) {
+  var seasonDim = ndx.dimension(dc.pluck("Season"));
+  averageSpeadPerSeasonDim = seasonDim.group().reduceSum(function(d) {
+    return d.Transfer_fee;
+  });
+  console.log(averageSpeadPerSeasonDim.all());
+  dc.lineChart("#line_graph")
+    .width(600)
+    .height(550)
+    .margins({ top: 10, right: 40, bottom: 75, left: 75 })
+    .dimension(seasonDim)
+    .group(averageSpeadPerSeasonDim)
+    .x(d3.scaleBand())
+    .xUnits(dc.units.ordinal)
+    .renderHorizontalGridLines(true)
+    .curve(d3.curveCatmullRom.alpha(0.5))
+    .renderArea(true)
+    .renderDataPoints(true)
+    .colors("black")
+    .xAxisLabel("Seasons")
+    .yAxisLabel("Transfer Fee")
+    .yAxis()
+    .tickFormat(euroFormat);
+}
