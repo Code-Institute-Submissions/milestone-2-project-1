44fbbed32e6e325957d7ada7793c0f7efc7333b3
plyn85 <plyn99@gmail.com>
2019-10-22 12:11:58 +0100

    adding button the hides callout section an shows data

---
+++ static/js/graph.js
@@ -0,0 +1,8 @@
+// jquery
+//targeting button in callout section
+//here that will hide the callout section when clicked an shoe that
+$(document).ready(function() {
+  $("#data_btn_callout").click(function() {
+    $("#callout_text").hide();
+  });
+});
